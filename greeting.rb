082b3b115eec1955e06b51fe4930976f07f5b55a
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-16 03:05:51 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,7 @@
+def greeting_a_person(name, language)
+  puts "wazzzzzzup, #{name}. We heard you are a great #{language} programmer."
+end
+
+name = "permethius"
+language = "ruby"
+greeting_a_person(name,language)
